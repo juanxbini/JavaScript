2fe741f836bf5494579faacbf46bc98f4230881a
juanxbini <juanxbini@gmail.com>
2023-05-03 15:24:45 -0300

    Asignar un evento load a una imagen.gif

---
+++ educit-js-adv-clase-4/implementacion/index.js
@@ -0,0 +1,7 @@
+// Asignar un eveto load a una imagen.gif
+
+// Obtener nodo de etiqueta con id 'gif' y asignarle un evento 'load'
+
+let gif = document.querySelector('#gif');
+
+gif.addEventListener('load', ()=>{})
