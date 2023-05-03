639227fa90ec4aa27fb47d1d080b65e7e105aff6
juanxbini <juanxbini@gmail.com>
2023-05-03 17:45:27 -0300

    Cuando el .gif haya terminado de cargar hacer un pedido AJAX para ir a buscar los contenidos del archivo home.html

--- educit-js-adv-clase-4/implementacion/index.js
+++ educit-js-adv-clase-4/implementacion/index.js
@@ -4,4 +4,35 @@
 
 let gif = document.querySelector('#gif');
 
-gif.addEventListener('load', ()=>{})
+// Crear funcion getDocument
+function getDocument(url,onload){
+    let xhr = new XMLHttpRequest()
+    xhr.responseType = 'document'
+    xhr.open('get', url)
+    xhr.addEventListener('load', ()=>{
+        if (xhr.status == 200){
+            onload(xhr.response, false)
+        }
+        if(xhr.status == 404){
+            onload('404 not found', true)
+        }
+    })
+    xhr.send()
+}
+
+/* Cuando el .gif haya terminado de cargar hacer un pedido 
+AJAX para ir a buscar los contenidos del archivo home.html:*/
+
+gif.addEventListener('load', (e)=>{
+    setTimeout(()=>{
+        getDocument('./src/plantillas/home.html', (data,err)=>{
+            if(err){
+                console.log('Ha ocurrido un error: ', data)
+                return
+            }else{
+                console.log('Success: ', data)
+            }
+        })
+    },4000)
+    
+})
