c7962634f13a7af42eedb46d5dec8a6a7cc83d08
juanxbini <juanxbini@gmail.com>
2023-05-03 18:25:43 -0300

    Al finalizar el pedido mostrar el contenido del resultado dentro del elemento <main>:

--- educit-js-adv-clase-4/implementacion/index.js
+++ educit-js-adv-clase-4/implementacion/index.js
@@ -7,7 +7,7 @@ let gif = document.querySelector('#gif');
 // Crear funcion getDocument
 function getDocument(url,onload){
     let xhr = new XMLHttpRequest()
-    xhr.responseType = 'document'
+    xhr.responseType = 'text/html'
     xhr.open('get', url)
     xhr.addEventListener('load', ()=>{
         if (xhr.status == 200){
@@ -23,6 +23,15 @@ function getDocument(url,onload){
 /* Cuando el .gif haya terminado de cargar hacer un pedido 
 AJAX para ir a buscar los contenidos del archivo home.html:*/
 
+// Al finalizar el pedido mostrar el contenido del resultado dentro del elemento <main>:
+
+function renderDocument(data){
+    let plantilla = data
+    let nodo = document.createElement('div')
+    nodo.innerHTML = plantilla
+    document.body.appendChild(nodo)
+}
+
 gif.addEventListener('load', (e)=>{
     setTimeout(()=>{
         getDocument('./src/plantillas/home.html', (data,err)=>{
@@ -30,7 +39,7 @@ gif.addEventListener('load', (e)=>{
                 console.log('Ha ocurrido un error: ', data)
                 return
             }else{
-                console.log('Success: ', data)
+                renderDocument(data)
             }
         })
     },4000)
